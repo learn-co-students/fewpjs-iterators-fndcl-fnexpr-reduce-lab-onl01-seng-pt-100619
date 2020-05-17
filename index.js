cf439a4279a2e6ef2655c5afa7d17d88c6452f92
github username <github email address>
2020-05-17 15:35:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries = batteryBatches.reduce(function(total, batches){
+  return batches + total}, 0)
+
+
+// function getTotalAmountForProducts(products) {
+//   let totalPrice = 0;
+ 
+//   products.forEach(function(product) {
+//     totalPrice += product.price;
+//   });
+ 
+//   return totalPrice;
+// }
